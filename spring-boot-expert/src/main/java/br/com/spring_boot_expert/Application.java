3be3bb8a760ffeedee038228aef066bd7dc8b1ba
Paulo-vitorCS <paulo.vitorcs@hotmail.com>
2024-08-02 16:04:58 -0300

    Migration to MySQL database

--- spring-boot-expert/src/main/java/br/com/spring_boot_expert/Application.java
+++ spring-boot-expert/src/main/java/br/com/spring_boot_expert/Application.java
@@ -6,14 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-//	@Bean
-//	public CommandLineRunner commandLineRunner(@Autowired ClientRepository clientRepository) {
-//		return args-> {
-//			Client c = new Client("Paulo Vitor");
-//			clientRepository.save(c);
-//		};
-//	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- spring-boot-expert/src/main/java/br/com/spring_boot_expert/domain/OrderItem.java
+++ spring-boot-expert/src/main/java/br/com/spring_boot_expert/domain/OrderItem.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 public class OrderItem {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
